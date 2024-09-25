e1e708fc67453e9bb51b39a3033c696766a0ab9f
BoomTHDev <minecraft8231@gmail.com>
2024-09-25 16:00:34 +0700

    try disabled rule

--- app/check-battery/CheckBatteryComp.tsx
+++ app/check-battery/CheckBatteryComp.tsx
@@ -1,5 +1,3 @@
-// @ts-nocheck
-
 'use client'
 
 import React, { useEffect, useState } from 'react'
@@ -22,7 +20,7 @@ export default function CheckBatteryComp() {
         })
 
         if ('getBattery' in navigator) {
-            (navigator).getBattery().then((battery) => {
+            (navigator as any).getBattery().then((battery: any) => {
                 const sendBatteryStatus = () => {
                     socketIo.emit('battery_status', {
                         level: battery.level * 100,
